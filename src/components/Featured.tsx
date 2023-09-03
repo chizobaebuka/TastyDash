cde6101611c853b4a586dfd57962eee8bdcbc6fe
Decagon <decagon@Martins.local>
2023-09-03 18:44:57 +0100

    done with the featured section of the app

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import { featuredProducts } from '@/data'
+import Image from 'next/image'
 
 const Featured = () => {
     return (
-        <div>Featured</div>
+        <div className='w-screen overflow-x-scroll text-red-500 '>
+            {/* WRAPPER */}
+            <div className='w-max flex'>
+                {featuredProducts.map((item) => (
+                    <div key={item.id} className='w-screen h-[60vh] flex flex-col items-center justify-around p-4 hover: bg-fuchsia-50 transition-all duration-300 md:w-[50vw] xl:w-[32vw] xl:h-[90vh]'>
+                        {/* IMAGE CONTAINER */}
+                        {item.img && (
+                            <div className='relative flex-1 w-full hover:rotate-[60deg] transition-all duration-500'>
+                                <Image src={item.img} alt="" fill className="object-contain"/>
+                            </div>
+                        )}
+                        {/* TEXT CONTAINER */}
+                        <div className="flex-1 flex flex-col items-center justify-center text-center gap-4">
+                            <h1 className="text-xl font-bold uppercase xl:text-2xl 2xl:text-3xl">{item.title}</h1>
+                            <p className='p-4 2xl:p-8 '>{item.desc}</p>
+                            <span>${item.price}</span>
+                            <button className='bg-red-500 text-white p-2 rounded-md'>Add To Cart</button>
+                        </div>
+                    </div>
+                ))}
+                {/* SiNGLE ITEMS */}
+            </div>
+        </div>
     )
 }
 
